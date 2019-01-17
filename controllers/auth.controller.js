5be4d5255f2ea27fd8b07214b86c81e6e40424c4
Zayne  Berndt <berndt.zayne@gmail.com>
2019-01-17 16:42:54 +0000

    async function testing

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,13 @@
-function AuthController(){
-  
+ function AuthController(){
+
   function isAuthorized(roles, neededRole){
-    return roles.indexOf(neededRole) >= 0;
+    return roles.indexOf(neededRole) >=0;
   }
-  return {isAuthorized};
+
+  function isAuthorizedAsync(roles, neededRole, cb){
+    setTimeout(function(){cb(roles.indexOf(neededRole) >= 0)}, 0);
+  }
+  return {isAuthorized, isAuthorizedAsync};
 }
 
 module.exports = AuthController();
