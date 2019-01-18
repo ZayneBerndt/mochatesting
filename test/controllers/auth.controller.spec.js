2c4365d18cca1c1af92f329e52c4b258d683e611
Zayne  Berndt <berndt.zayne@gmail.com>
2019-01-18 12:25:40 +0000

    code tidy up

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,23 +1,36 @@
 var assert = require('assert');
-var authController = require('../../controllers/auth.controller.js')
+var authController = require('../../controllers/auth.controller');
 
-describe('AuthController', function(){
-  describe('isAuthorized', function(){
-      it('Should return false if not authorized', function(){
-          assert.equal(false, authController.isAuthorized(['user'], 'admin'));
-    })
-      it('Should return true if authorized', function(){
-          assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'));
-    })
-  })
-  describe('isAuthorizedAsync', function(){
-      it('Should return false if not authorized', function(done){
-        authController.isAuthorizedAsync(['user'], 'admin',
-        function(isAuth){
-          assert.equal(false, isAuth);
-          done();
-      });
 
+
+describe('AuthController', function () {
+    beforeEach(function settingUpRoles() {
+        console.log('running before each');
+        authController.setRoles(['user']);
+    });
+
+    describe('isAuthorized', function () {
+
+        it('Should return false if not authorized', function () {
+            assert.equal(false, authController.isAuthorized('admin'));
+        })
+        it('Should return true if authorized', function () {
+            authController.setRoles(['user', 'admin']);
+            assert.equal(true, authController.isAuthorized('admin'));
+        })
+        it('should not allow a get if not authorized');
+        it('should allow get if authorized');
     })
-  })
+    describe('isAuthorizedAsync', function () {
+
+        it('Should return false if not authorized', function (done) {
+            authController.isAuthorizedAsync('admin',
+                function (isAuth) {
+                    assert.equal(false, isAuth);
+                    done();
+                });
+
+        })
+    })
+
 });
